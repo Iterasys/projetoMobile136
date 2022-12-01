6d4c78508a0f331e59f3f48bb535aa52ee7acde5
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-11-30 22:04:01 -0300

    Importacao do codigo gerado pelo Appium Inspector

--- test/specs/selecionarProduto.spec.js
+++ test/specs/selecionarProduto.spec.js
@@ -1,9 +1,23 @@
 // Em breve um codigo aqui   
 
 const wdio = require('webdriverio');
-const caps = {"platformName":"Android","appium:platformVersion":"9.0","appium:appiumVersion":"1.22.0","appium:deviceName":"Samsung Galaxy S9 FHD GoogleAPI Emulator","appium:deviceOrientation":"portrait","appium:app":"storage:filename=mda-1.0.15-18.apk","appium:appPackage":"com.saucelabs.mydemoapp.android","appium:appActivity":"com.saucelabs.mydemoapp.android.view.activities.SplashActivity","appium:ensureWebviewsHavePages":true,"appium:nativeWebScreenshot":true,"sauce:options":{"name":"Appium Desktop Session -- Nov 30, 2022 7:25 PM"},"appium:newCommandTimeout":3600,"appium:connectHardwareKeyboard":true};
+const caps = {
+              "platformName":"Android",
+              "appium:platformVersion":"9.0",
+              "appium:appiumVersion":"1.22.0",
+              "appium:deviceName":"Samsung Galaxy S9 FHD GoogleAPI Emulator",
+              "appium:deviceOrientation":"portrait",
+              "appium:app":"storage:filename=mda-1.0.15-18.apk",
+              "appium:appPackage":"com.saucelabs.mydemoapp.android",
+              "appium:appActivity":"com.saucelabs.mydemoapp.android.view.activities.SplashActivity",
+              "appium:ensureWebviewsHavePages":true,
+              "appium:nativeWebScreenshot":true,
+              "sauce:options":{"name":"Appium Desktop Session -- Nov 30, 2022 7:25 PM"},
+              "appium:newCommandTimeout":3600,
+              "appium:connectHardwareKeyboard":true
+            };
 const driver = wdio.remote({
-  protocol: "http",
+  protocol: "https",
   host: "ondemand.us-west-1.saucelabs.com",
   port: 80,
   path: "/wd/hub",
